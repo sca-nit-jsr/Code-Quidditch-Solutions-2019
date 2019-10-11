133b60ba8d236f7159566f8bc068aa76693b90fd
Deepak Chauhan <deepakchauhan878@gmail.com>
2019-10-11 12:49:37 +0530

    Corrected code for Excited Sunny!!!

--- Excited Sunny !!!/Python3/Deepak Chauhan.py
+++ Excited Sunny !!!/Python3/Deepak Chauhan.py
@@ -1,12 +1,10 @@
 ## Author : Deepak Chauhan
 ## GitHub Profile : https://Github.com/Royaleagle73 
-
-## !!!! This Solution is Not Working Right now !!!
-from math import sqrt
+import math
 n = int(input())
 for _ in range(n):
     l,r = [int(i) for i in input().split()]
-    total = int(sqrt(r)) - int(sqrt(l+1)) + 1
+    total = math.floor(math.sqrt(r)) - math.ceil(math.sqrt(l)) + 1
     if(total%2!=0):
         print("Odd")
     else:
