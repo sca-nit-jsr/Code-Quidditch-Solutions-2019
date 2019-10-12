1ab1febb12978a6c872767220664a43822bf39ec
shubham goswami <40126673+shubham2704@users.noreply.github.com>
2019-10-12 19:34:00 +0530

    solution for special price in python

---
+++ Special Price/python/specialPrice.py
@@ -0,0 +1,36 @@
+
+size = 100001
+n = int(raw_input())
+
+arr = []
+
+for i in range(0,n):
+	i = int(raw_input())
+	arr.append(i)
+
+query = int(raw_input())
+
+hash = [0]*size
+
+for j in range(0,n):
+	if(arr[j]<size):
+		hash[arr[j]] = 1 + hash[arr[j]]
+
+for k in hash:
+	if (hash[k]>0):
+		hash[k] = k * hash[k]
+
+for l in hash:
+
+	hash[l] = 1 + hash[l-1]
+
+while query!=0:
+	price1 = int(raw_input())
+	price2 = int(raw_input())
+
+	if(price1>0):
+		print(hash[price2]-hash[price1-1])
+	else:
+		print(hash[price2])
+		
+	query = query-1
