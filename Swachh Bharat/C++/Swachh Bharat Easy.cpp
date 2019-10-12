31a1550c22e8035ce880c6f12c0c207460e73049
Cyberkid2311 <46487087+Cyberkid2311@users.noreply.github.com>
2019-10-12 12:25:43 +0530

    Create Swachh Bharat Easy.cpp

---
+++ Swachh Bharat/C++/Swachh Bharat Easy.cpp
@@ -0,0 +1,30 @@
+/*Author Name: Balasubramanian R
+  Github Link: https://github.com/Cyberkid2311 */
+  
+/*  It forms a series 2,8,24,64....
+    which can be expressed as 1 * (2)^1, 2 * (2)^2, 3 * (2)^3,... */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define mod 1000000007
+int main()
+{
+	ll n;
+        cin >> n;
+        ll arr[n+1], x = 1;
+        for(int i=1; i<=n; i++)
+        {
+        	x = (x * 2)%mod;      //In Every step x is multiplied with 2 to reached the desired power.
+                arr[i] = x;     //The desired power of 2 is saved to the array.
+        }
+        ll ans = 0;
+        for(int i=1; i<=n; i++)
+	{
+ 	        ans += (arr[i]*i)%mod;  /* The power of 2 previously stored in the array is multiplied with its index 
+                                    and the sum is calculated. */
+        }
+        cout << ans%mod;
+	return 0;
+}
